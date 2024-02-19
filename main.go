76cacdf5afaeb1349ccfe2c55a52121533bdc248
vhast <fedvhast@gmail.com>
2024-02-19 11:34:12 +0200

    201 POST Request working

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 type todo struct {
 	ID        string `json:"id"`
-	Item      string `json:"title"`
+	Item      string `json:"item"`
 	Completed bool   `json:"completed"`
 }
 
@@ -22,8 +22,20 @@ func getTodos(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, todos)
 }
 
+func addTodo(context *gin.Context) {
+	var newTodo todo
+	if err := context.BindJSON(&newTodo); err != nil {
+		return
+	}
+
+	todos = append(todos, newTodo)
+
+	context.IndentedJSON(http.StatusCreated, newTodo)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/todos", getTodos)
+	router.POST("/todos", addTodo)
 	router.Run("localhost:9090")
 }
